114fb180d261dad43dd83c62694904d362f523c0
Mathias Brun <brun.mathias@email.de>
2018-08-31 21:20:45 +0200

    Updated javadoc

--- src/main/java/com/github/mbrun/proxymetrics/InvocationMetricsProxy.java
+++ src/main/java/com/github/mbrun/proxymetrics/InvocationMetricsProxy.java
@@ -6,7 +6,10 @@ import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * See doc at https://docs.oracle.com/javase/8/docs/technotes/guides/reflection/proxy.html
+ * Class usable for measuring method invocation counts and times of interface methods implemented by any class and instance.
+ * Usage of java dynamic proxy functionality for simple injection.
+ *
+ * @author M. Brun
  */
 public class InvocationMetricsProxy implements InvocationHandler {
 
@@ -16,10 +19,12 @@ public class InvocationMetricsProxy implements InvocationHandler {
     private Object target;
 
     /**
-     * @param obj
+     * Creates a new proxy object which measures all methods implemented by all interfaces of the given object. The provided {@code marker} object serves for identification purpose of the {@link InvocationMetricsProxy}. Make sure that the marker object has a {@code hashCode()} implementation that provides unique not changing values.
+     *
+     * @param obj    The object to create the proxy for. All implemented interface methods of the given class will be captured by metrics measurement
      * @param marker Object to be used as marker to later retrieve the measurements in form of an {@link InvocationMetricsProxy} object. This object must use a not changing proper implementation of {@code hashcode()} method as a hash based map is used in the background!
-     * @param <T>
-     * @return
+     * @param <T>    type of the passed in object that shall be reflected by the proxy
+     * @return Returns the newly created proxy object that can be used instead of the original passed in object for invocation.
      */
     @SuppressWarnings("unchecked")
     public static <T> T newInstance(T obj, Object marker) {
@@ -33,18 +38,20 @@ public class InvocationMetricsProxy implements InvocationHandler {
     }
 
     /**
-     * @param marker
-     * @return
+     * Provides access to the set of created {@link InvocationMetricsProxy} instances and returns the related instance by providing the formerly used marker object (see {@link InvocationMetricsProxy#newInstance(Object, Object)}).
+     *
+     * @param marker object used to identify the specific {@link InvocationMetricsProxy} instance (see {@link InvocationMetricsProxy#newInstance(Object, Object)})
+     * @return Returns an {@link Optional} that contains the desired instance if found.
      */
     public static Optional<InvocationMetricsProxy> getMetricsProxy(Object marker) {
         return Optional.ofNullable(objectProxyMap.get(marker));
     }
 
     /**
-     * Utility method to log the statistics of a single {@link LongSummaryStatistics} object with a optional preliminary lines.
+     * Provides a directly loggable string of a single {@link LongSummaryStatistics} object with optional preliminary lines.
      *
-     * @param statistics
-     * @param additionals
+     * @param statistics  the statistics to be loggable
+     * @param additionals additional string array as preliminary log content. Each array entry will be on a dedicated line
      */
     public static String getLoggable(LongSummaryStatistics statistics, String... additionals) {
         StringJoiner joiner = new StringJoiner("\n");
@@ -81,10 +88,12 @@ public class InvocationMetricsProxy implements InvocationHandler {
     }
 
     /**
-     * @param methodName
-     * @param containsSearch
-     * @param stopOnFirstMatch
-     * @return
+     * Provides a list of found methods and according methods.
+     *
+     * @param methodName       Name of the method to log the statistics for. May be used to specify only parts of a method name. See {@code containsSearch} parameter.
+     * @param containsSearch   Flag to define if the given {@code methodName} shall be used for a contains search among all the methods or not.
+     * @param stopOnFirstMatch Flag to define if the log of statistics shall be stopped after finding the first matching method.
+     * @return Returns a list of all found entries matching to the given search criterias. The return will be for sure not be null.
      */
     public List<Map.Entry<String, LongSummaryStatistics>> getStatsForMethod(String methodName,
                                                                             boolean containsSearch, boolean stopOnFirstMatch) {
@@ -102,7 +111,9 @@ public class InvocationMetricsProxy implements InvocationHandler {
     }
 
     /**
-     * Logs the statistics of all methods.
+     * Provides the metrics of all methods in a loggable way.
+     *
+     * @return Returns a directly loggable string with all method invocation metrics
      */
     public String getAllLoggable() {
         StringJoiner joiner = new StringJoiner("\n");
@@ -110,14 +121,16 @@ public class InvocationMetricsProxy implements InvocationHandler {
             joiner.add(InvocationMetricsProxy.getLoggable(stats.getValue(), "++++ Stats for " + stats.getKey() + " ++++"));
         }
         return joiner.toString();
+
     }
 
     /**
-     * Logs the statistics of one or more methods.
+     * Provides the metrics of one or more methods in a loggable way.
      *
      * @param methodName       Name of the method to log the statistics for. May be used to specify only parts of a method name. See {@code containsSearch} parameter.
      * @param containsSearch   Flag to define if the given {@code methodName} shall be used for a contains search among all the methods or not.
      * @param stopOnFirstMatch Flag to define if the log of statistics shall be stopped after finding the first matching method.
+     * @return Returns a directly loggable string with the found method invocation metrics
      */
     public String getLoggable(String methodName, boolean containsSearch, boolean stopOnFirstMatch) {
         List<Map.Entry<String, LongSummaryStatistics>> stats = getStatsForMethod(methodName, containsSearch, stopOnFirstMatch);
